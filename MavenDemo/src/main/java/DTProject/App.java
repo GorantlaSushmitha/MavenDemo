08656addc0007bc9a209627175a6232cb5655777
gsush <gsush@LAPTOP-HPJHJJ4F>
2017-04-19 12:44:08 +0530

    MavenDemo

---
+++ MavenDemo/src/main/java/DTProject/App.java
@@ -0,0 +1,17 @@
+package DTProject;
+
+import javax.servlet.RequestDispatcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
